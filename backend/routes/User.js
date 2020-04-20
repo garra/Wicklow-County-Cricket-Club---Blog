c669992696976bcda2737a60d621200698e43234
gareth douglas <gareth.douglas@gmail.com>
2020-04-20 20:56:44 +0100

    Update User.js

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -6,15 +6,15 @@ const rateLimit = require("express-rate-limit");
 const router     = express.Router()
 
 const createAccountLimiter = rateLimit({
-    windowMs: 60 * 60 * 1000, // 1 hour window
-    max: 5, // start blocking after 5 requests
+    windowMs: 60 * 60, // 1 hour window
+    max: 10, // start blocking after 5 requests
     message: "Too many accounts created from this IP, please try again after an hour"
   });
 
   
 const verifyAccountLimiter = rateLimit({
-    windowMs: 60 * 60 * 1000, // 1 hour window
-    max: 5, // start blocking after 5 requests
+    windowMs: 60 * 60, // 1 hour window
+    max: 10, // start blocking after 5 requests
     message: "Too many logins from this IP, please try again after an hour"
   });
   
